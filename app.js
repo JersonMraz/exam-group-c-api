16acd334ef7ccc4e0038774c0ac740cc1b3a2da0
unknown <YY549@ccs.local>
2025-04-05 14:55:38 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const examRoutes = require('./routes/exams');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Use the exams routes
+app.use('/', examRoutes);
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
